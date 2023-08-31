7b63de078542bd35bf874386c5e6439ed9bf3b91
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-31 12:15:15 +0300

    Complete Assignent

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-
+# Import necessary modules
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
+#Create a declarative base
 Base = declarative_base()
 
+#Define the student class
 class Student(Base):
-    pass
+    #Define students table name
+    __tablename__ =  'students'
 
+#Define columns for the table
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
+
+#Check if the script is being run as the main program
 if __name__ == '__main__':
-    pass
+    #Create a db engine using SQLite and connect it to the students.db
+    engine = create_engine('sqlite:///students.db')
+    #Create the tables define by the classes that inherit from Base
+    Base.metadata.create_all((engine))
